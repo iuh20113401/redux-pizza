97d248be620e17a810b8184aeb8f2844ca6c796a
iuh20113401 <kiet5454aa@gmail.com>
2024-06-10 15:43:36 +0700

    first commit

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,7 +3,7 @@ import Button from "../../ui/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { addCart, getItemQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
-import UpdateQuantity from "../cart/UdateQuantity";
+import UpdateQuantity from "../cart/UpdateQuantity";
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
   const dispatch = useDispatch();
